6d0364985cfeedda7203b5a67ca8ec06d36894e9
Janar Juusu <janar@juusujanar.eu>
2021-11-24 12:24:07 +0200

    Update the plugin to a bit newer standards
    Signed-off-by: Janar Juusu <janar@juusujanar.eu>

--- README.md
+++ README.md
@@ -1,15 +1,58 @@
-# asdf-dbmate
+<div align="center">
 
-![CI](https://github.com/juusujanar/asdf-dbmate/workflows/Test/badge.svg?branch=master)
+# asdf-dbmate [![Build](https://github.com/juusujanar/asdf-dbmate/actions/workflows/build.yml/badge.svg)](https://github.com/juusujanar/asdf-dbmate/actions/workflows/build.yml) [![Lint](https://github.com/juusujanar/asdf-dbmate/actions/workflows/lint.yml/badge.svg)](https://github.com/juusujanar/asdf-dbmate/actions/workflows/lint.yml)
 
-[dbmate](https://github.com/amacneil/dbmate) plugin for [asdf](https://github.com/asdf-vm/asdf) version manager
 
-## Install
+[dbmate](https://github.com/amacneil/dbmate) plugin for the [asdf version manager](https://asdf-vm.com).
 
+</div>
+
+# Contents
+
+- [Dependencies](#dependencies)
+- [Install](#install)
+- [Contributing](#contributing)
+- [License](#license)
+
+# Dependencies
+
+- `bash`, `curl`: generic POSIX utilities.
+
+# Install
+
+Plugin:
+
+```shell
+asdf plugin add dbmate
+# or
+asdf plugin add dbmate https://github.com/juusujanar/asdf-dbmate.git
 ```
-asdf plugin-add dbmate https://github.com/juusujanar/asdf-dbmate.git
+
+dbmate:
+
+```shell
+# Show all installable versions
+asdf list-all dbmate
+
+# Install specific version
+asdf install dbmate latest
+
+# Set a version globally (on your ~/.tool-versions file)
+asdf global dbmate latest
+
+# Now dbmate commands are available
+dbmate --version
 ```
 
-## Use
+Check [asdf](https://github.com/asdf-vm/asdf) readme for more instructions on how to
+install & manage versions.
 
-Check out the [asdf](https://github.com/asdf-vm/asdf) readme for instructions on how to install and manage tools.
+# Contributing
+
+Contributions of any kind welcome! See the [contributing guide](contributing.md).
+
+[Thanks goes to these contributors](https://github.com/juusujanar/asdf-dbmate/graphs/contributors)!
+
+# License
+
+See [LICENSE](LICENSE) © [Janar Juusu](https://github.com/juusujanar/)
---
+++ lib/utils.bash
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+GH_REPO="https://github.com/amacneil/dbmate"
+TOOL_NAME="dbmate"
+TOOL_TEST="dbmate --version"
+
+fail() {
+  echo -e "asdf-$TOOL_NAME: $*"
+  exit 1
+}
+
+curl_opts=(-fsSL)
+
+# NOTE: You might want to remove this if dbmate is not hosted on GitHub releases.
+if [ -n "${GITHUB_API_TOKEN:-}" ]; then
+  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+fi
+
+sort_versions() {
+  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+}
+
+list_github_tags() {
+  git ls-remote --tags --refs "$GH_REPO" |
+    grep -o 'refs/tags/.*' | cut -d/ -f3- |
+    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+}
+
+list_all_versions() {
+  # Change this function if dbmate has other means of determining installable versions.
+  list_github_tags
+}
+
+download_release() {
+  local version filename url platform arch
+  version="$1"
+  filename="$2"
+  platform="$(get_platform)"
+  arch="$(get_arch)"
+
+  # "https://github.com/amacneil/dbmate/releases/download/v${version}/${filename}"
+  url="$GH_REPO/releases/download/v${version}/dbmate-${platform}-${arch}"
+
+  echo "* Downloading $TOOL_NAME release $version..."
+  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+  chmod +x "$filename"
+}
+
+install_version() {
+  local install_type="$1"
+  local version="$2"
+  local install_path="$3"
+
+  if [ "$install_type" != "version" ]; then
+    fail "asdf-$TOOL_NAME supports release installs only"
+  fi
+
+  (
+    mkdir -p "$install_path/bin"
+    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path/bin/"
+
+    local tool_cmd
+    tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+    test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
+
+    echo "$TOOL_NAME $version installation was successful!"
+  ) || (
+    rm -rf "$install_path"
+    fail "An error occurred while installing $TOOL_NAME $version."
+  )
+}
+
+get_platform() {
+  local silent=${1:-}
+  local platform=""
+
+  platform="$(uname | tr '[:upper:]' '[:lower:]')"
+
+  case "$platform" in
+  linux*)
+    local platform=linux
+    ;;
+  darwin*)
+    local platform=macos
+    ;;
+  *)
+    err "Platform '${platform}' not supported!"
+    exit 1
+    ;;
+  esac
+
+  echo -n "$platform"
+}
+
+get_arch() {
+  local arch=""
+
+  case "$(uname -m)" in
+  # List here the supported architectures
+  x86_64 | amd64) arch="amd64" ;;
+  aarch64 | arm64) arch="arm64" ;;
+  *)
+    err "Arch '$(uname -m)' not supported!"
+    exit 1
+    ;;
+  esac
+
+  echo -n $arch
+}
